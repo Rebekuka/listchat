87356c0d1f63d222199c20259a86ed369d0317c5
rebecca biberian <rebeccabiberian@gmail.com>
2022-04-24 21:54:07 +0200

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Cat.destroy_all
+Cat.create(name: "vincent", race: "léopard des neiges", photo_url: "léopard.jpg", rating: 9)
+Cat.create(name: "gustave", race: "persan", photo_url: "persan.jpg", rating: 5)
+Cat.create(name: "rebecca", race: "pallas", photo_url: "pallas.jpg", rating: 3)
+Cat.create(name: "bob", race: "maine-coon", photo_url: "main-coon.jpg", rating: 6)
+Cat.create(name: "peach", race: "bengal", photo_url: "bengal", rating: 5)
